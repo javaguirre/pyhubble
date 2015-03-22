a0e474bc4097d273e45bfdde934b61af5b9a0ece
Javier Aguirre <contacto@javaguirre.net>
2015-03-22 18:01:08 +0100

    Version 0.1.1 bump
    The install works for the first time, I removed the README.read and
    LICENSE.read from setup.py, people searching for that will be able to
    get that information from their respective files

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@ from distutils.core import setup
 
 setup(
     name='PyHubble',
-    version='0.1',
+    version='0.1.1',
     author='Javier Aguirre',
     author_email='hello@javaguirre.net',
     packages=['pyhubble'],
     url='https://github.com/javaguirre/pyhubble',
-    download_url='https://github.com/javaguirre/pyhubble/tarball/0.1',
-    license=open('LICENSE.txt').read(),
+    license='GPLv3',
     description='A python client for hubble',
-    long_description=open('README.md').read(),
+    long_description='',
     install_requires=['requests'],
-    keywords=['hubble'],
-    classifiers=[],
+    keywords=['hubble']
 )
